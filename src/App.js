d9644877cb3c71f9242debc888ccd971ae1f4636
akhileshtiwari2 <akhilesh.tiwari02@gmail.com>
2023-09-09 09:13:02 +0530

    Routing add

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Nav from './Nav';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<h1>Product Listing Component</h1>}/>
+        <Route path="/add" element={<h1>Add Product Component</h1>}/>
+        <Route path="/update" element={<h1>Update Product Component</h1>}/>
+        <Route path="/logout" element={<h1>logout</h1>}/>
+        <Route path="/profile" element={<h1>Profile</h1>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
