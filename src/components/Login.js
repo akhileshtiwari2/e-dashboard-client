d3bac86834fb09051c3cf0aea0865e62286f01ca
akhileshtiwari2 <akhilesh.tiwari02@gmail.com>
2023-09-10 19:49:37 +0530

    Login API integration

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,61 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Login=()=> {
-    const [email,setEmail]= React.useState('')
-    const [password,setPassword]= React.useState('')
-    const handleLogin=()=>{
-      console.warn(email,password)
+const Login = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const auth = localStorage.getItem("user");
+    if (auth) {
+      navigate("/");
     }
+  });
+  const handleLogin = async () => {
+    console.warn(email, password);
+    let result = await fetch("http://localhost:5000/login", {
+      method: "post",
+      body: JSON.stringify({ email, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    result = await result.json();
+    console.log(result);
+    if (result.name) {
+      localStorage.setItem("user", JSON.stringify(result));
+      navigate("/");
+    } else {
+      alert("please enter correct details");
+    }
+  };
   return (
     <div className="login-box">
-          <h1>Login</h1>
+      <h1>Login</h1>
 
-        <input className="input-box" type="text" placeholder='Enter Email'
-        onChange={(e)=>setEmail(e.target.value)} value={email} />
+      <input
+        className="input-box"
+        type="text"
+        placeholder="Enter Email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
 
-        <input className="input-box" type="password" placeholder='Enter Password'
-        onChange={(e)=>setPassword(e.target.value)} value={password}/>
+      <input
+        className="input-box"
+        type="password"
+        placeholder="Enter Password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
 
-        <button onClick={handleLogin} className="login-btn" type="button">Login</button>
-         
+      <button onClick={handleLogin} className="login-btn" type="button">
+        Login
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import {Link,useNavigate} from 'react-router-dom';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-const Nav=()=>{
-    const auth = localStorage.getItem('user');
-    const navigate = useNavigate();
-    const logout=()=>{
-        localStorage.clear();
-        navigate('/signup')
-    }
-    return(
-        <div>
-            <ul className="nav-ul">
-                <li><Link to="/">Products</Link></li>
-                <li><Link to="/add">Add Product</Link></li>
-                <li><Link to="/update">Update Products</Link></li>
-                <li><Link to="/profile">Profile</Link></li>
-                <li>{auth?<Link onClick={logout} to="/logout">Logout</Link>:<Link to="/signup">SignUp</Link>}</li>
-                <li><Link to="/login">Login</Link></li>
-            </ul>
-        </div>
-    )
-}
+const Nav = () => {
+  const auth = localStorage.getItem("user");
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.clear();
+    navigate("/signup");
+  };
+  return (
+    <div>
+      <ul className="nav-ul">
+        <li>
+          <Link to="/">Products</Link>
+        </li>
+        <li>
+          <Link to="/add">Add Product</Link>
+        </li>
+        <li>
+          <Link to="/update">Update Products</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        {auth ? (
+          <li>
+            <Link onClick={logout} to="/logout">
+              Logout
+            </Link>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/signup">SignUp</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </div>
+  );
+};
 export default Nav;
