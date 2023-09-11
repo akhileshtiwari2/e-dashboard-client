983b3c468b92a4171ce07a8412bcb2b8cd4e5b55
akhileshtiwari2 <akhilesh.tiwari02@gmail.com>
2023-09-11 21:00:55 +0530

    Form validation

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -5,9 +5,15 @@ const AddProduct = () =>{
     const [price, setPrice] = useState("");
     const [category, setCategory] = useState("");
     const [company, setCompany] = useState("");
+    const [error,setError]= useState(false)
+
    
     const addProduct = async()=>{
-        console.log(name,price,category,company)
+        if(!name || !price || !category || !company)
+        {
+          setError(true)
+          return false;
+        }
         const userId = JSON.parse(localStorage.getItem('user'))._id;
         let result = await fetch("http://localhost:5000/add-product", {
       method: "post",
@@ -24,15 +30,19 @@ const AddProduct = () =>{
             <h1>Add Product</h1>
             <input className="input-box" type="text" placeholder="Enter product name" 
             onChange={(e)=>setName(e.target.value)} value={name} />
+            {error && !name && <span className="invalid-input">Enter valid name</span>}
 
             <input className="input-box" type="text" placeholder="Enter product price" 
             onChange={(e)=>setPrice(e.target.value)} value={price} />
+            {error && !price && <span className="invalid-input">Enter valid price</span>}
 
             <input className="input-box" type="text" placeholder="Enter product category"
             onChange={(e)=>setCategory(e.target.value)} value={category} />
+            {error && !category && <span className="invalid-input">Enter valid category</span>}
 
             <input className="input-box" type="text" placeholder="Enter product company"
             onChange={(e)=>setCompany(e.target.value)} value={company} />
+            {error && !company && <span className="invalid-input">Enter valid company</span>}
 
             <button onClick={addProduct} className="add-product-btn" type="button">Add Product</button>
         </div>
