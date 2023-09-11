6439812cc43116bb5bf40362e719e55faaa2272e
akhileshtiwari2 <akhilesh.tiwari02@gmail.com>
2023-09-11 19:44:06 +0530

    Add Product API Intigration Done

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -5,9 +5,20 @@ const AddProduct = () =>{
     const [price, setPrice] = useState("");
     const [category, setCategory] = useState("");
     const [company, setCompany] = useState("");
-    const addProduct = ()=>{
+   
+    const addProduct = async()=>{
         console.log(name,price,category,company)
-    }
+        const userId = JSON.parse(localStorage.getItem('user'))._id;
+        let result = await fetch("http://localhost:5000/add-product", {
+      method: "post",
+      body: JSON.stringify({ name,price,category,company,userId }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    result = await result.json();
+    console.log(result);
+  };
     return(
         <div className="product-container">
             <h1>Add Product</h1>
@@ -24,7 +35,6 @@ const AddProduct = () =>{
             onChange={(e)=>setCompany(e.target.value)} value={company} />
 
             <button onClick={addProduct} className="add-product-btn" type="button">Add Product</button>
-
         </div>
     )
 }
