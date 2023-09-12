6c18d27df8bc53b1b2d1f0c9fb99d759d98b39b3
akhileshtiwari2 <akhilesh.tiwari02@gmail.com>
2023-09-12 12:33:41 +0530

    Integrate Delete Product API

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -13,6 +13,16 @@ const ProductList=()=> {
         result = await result.json();
         setProducts(result);
     }
+    const deleteProduct=async (id)=>{
+    let result = await fetch(`http://localhost:5000/product/${id}`,{
+        method:"Delete"
+    });
+    result =await result.json()
+    if(result)
+    {
+        getProducts();
+    }
+    }
   return (
     <div className="product-list">
         <h1>Product List</h1>
@@ -22,15 +32,18 @@ const ProductList=()=> {
             <li>Price</li>
             <li>Category</li>
             <li>Company</li>
+            <li>Operation</li>
+
         </ul>
         {
             products.map((item,index)=>
-            <ul>
+            <ul key={item._id}>
             <li>{index+1}</li>
             <li>{item.name}</li>
             <li>${item.price}</li>
             <li>{item.category}</li>
             <li>{item.company}</li>
+            <li><button onClick={()=>deleteProduct(item._id)}>Delete</button></li>
             </ul>)
         }
     </div>
